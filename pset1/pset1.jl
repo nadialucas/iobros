f8bd857ce0f014a6394d094b49b9824f9f90a991
Nadia Lucas <nlucas040@gmail.com>
2021-02-02 01:41:42 -0600

    started gradient

--- pset1/pset1.jl
+++ pset1/pset1.jl
@@ -30,31 +30,40 @@ elseif pwd() == "/home/nrlucas"
 end
 main_data = CSV.read(string(data_path, "psetOne.csv"), DataFrame)
 
-# Part 8: Market t=17 (data setup is a bit janky for now)
-data17 = @where(main_data, :Market .== 17)
+## Helper function
+# take in dataset and market and return matrix of instruments, Xs, prices, shares, and ownership matrix
+function clean_data(df, mkt)
+    market = @where(df, :Market .== mkt)
+    # creates the ownership matrix
+    justbrands = @select(market, :Brand2, :Brand3)
+    justbrands = convert(Matrix, justbrands)
+
+    samebrand2 = [x1 == x2  ? 1 : 0 for x1 in justbrands[:,1], x2 in justbrands[:,1]  ]
+    samebrand3 = [y1 == y2 ? 1 : 0 for y1 in justbrands[:,2], y2 in justbrands[:,2]]
+    O = samebrand2 .* samebrand3
+
+    X_mat = @select(market, :Constant, :EngineSize, :SportsBike, :Brand2, :Brand3)
+    X = convert(Matrix, X_mat)
+    P_mat = @select(market, :Price)
+    P = convert(Matrix, P_mat)
+    S_mat = @select(market, :shares)
+    S = convert(Matrix, S_mat)
+    Z_mat = @select(market, :z1, :z2, :z3)
+    Z = convert(Matrix, Z_mat)
+    
+    return O, X, P, S, Z
+end
+
+# Part 8
+O_17, X_17, P_17, S_17, Z_17 = clean_data(main_data, 17)
 
 dist = Normal()
-data17 = @transform(data17, xi = rand(dist, nrow(data17)))
-#xi = [.838983, .931102, .385587, .885141, -.754398, 1.252868, .51293]
-#data17 = @transform(data17, xi = xi)
-
-# creates the ownership matrix
-justbrands = @select(data17, :Brand2, :Brand3)
-justbrands = convert(Matrix, justbrands)
-
-samebrand2 = [x1 == x2  ? 1 : 0 for x1 in justbrands[:,1], x2 in justbrands[:,1]  ]
-samebrand3 = [y1 == y2 ? 1 : 0 for y1 in justbrands[:,2], y2 in justbrands[:,2]]
-ownership_mat = samebrand2 .* samebrand3
+xi = rand(dist, length(X_17[:,1]))
+xi = reshape(xi, (length(xi), 1))
+X_17_xi = [X_17'; xi']'
 
 # now compute all shares taking price
 theta = [-3, 1, 1, 2, -1, 1, 1]
-
-
-data17_mat = @select(data17, :Constant, :EngineSize, :SportsBike, :Brand2, :Brand3, :xi)
-prices17_mat = @select(data17, :Price)
-prices17 = convert(Matrix, prices17_mat)
-xs = convert(Matrix, data17_mat)
-
 beta = theta[2:7]
 alpha = -1*theta[1]
 
@@ -99,7 +108,7 @@ function fp_solver(prices, beta, alpha, xs, ownership_mat)
     return p
 end
 
-homogenous_prices = fp_solver(prices17, beta, alpha, xs, ownership_mat)
+homogenous_prices = fp_solver(P_17, beta, alpha, X_17_xi, O_17)
 println(homogenous_prices)
 
 # we might need this later, who knows?
@@ -121,11 +130,7 @@ function hessian(p, beta, alpha, xs)
     return(H)
 end
 
-#### Time for problem 9 (CURRENTLY NOT WORKING, still unsure about the prob setup)
-
-function get_numerator(delta_j, sigma, X_j, zeta_i)
-    return exp.(delta_j+ sigma*X_j*zeta_i)
-end
+#### Problem 9
 
 function sHat(delta, X, sigma, zeta, I, J)
     vec_probs = zeros(J)
@@ -180,7 +185,9 @@ zeta = rand(dist, I, numChars)
 hey = sHat(delta, X, sigma, zeta, I, J)
 
 ### Problem 10: Inverse share function (this is from contraction mapping)
-function sHat_inverse(s, sigma, X, zeta, I, J, tol = 1e-14, maxiter = 10000)
+function sHat_inverse(s, sigma, X, zeta, I, J)
+    tol = 1e-14
+    maxiter = 10000
     # initialize delta
     delta = zeros(J)
     diff = 100
@@ -210,14 +217,55 @@ shares = sHat(delta, X, sigma, zeta, I, J)
 # woooo it works!!
 delts = sHat_inverse(shares, sigma, X, zeta, I, J)
 
+# Part 11 The Objective function
+# take in a sigma, the data (X, Z, S), the weighting matrix, and zeta draws
+# note that for now, just works in one market, could loop over multiple later
+function objective(sigma, X, Z, S, W, zeta)
+    I = length(zeta[:,1])
+    J = length(X[:,1])
+    # calculate delta
+    delta = sHat_inverse(S, sigma, X, zeta, I, J)
+    
 
-# Part 12
+    # calculate theta
+    bread = X' * Z * W * Z'
+    theta = (bread * X) \ bread * delta
 
+    # calculate xi
+    xi = delta - X*theta
+
+    result = (Z'*xi)' * W * Z' * xi
+    return result
+end
+
+numChars = 5
+sigma = .1 * (zeros(numChars, numChars) + Diagonal(ones(numChars)))
+zeta = rand(dist, I, numChars)
+W_17 = inv(Z_17'*Z_17)
+objective(sigma, X_17, Z_17, S_17, W_17, zeta)
+
+
+# Part 12 The gradient
+
+function jacobian_theta(theta_bar, sigma, X, S, zeta)
+end
+
+function jacobian_xi(theta_bar, sigma, P, X, zeta)
+    alpha = theta_bar[1]
+    beta = theta_bar[2:length(theta_bar)]
+    # get jacobian
+    jacobian_xi = get_jacobian(P, beta, alpha, X)
+
+end
+function gradient(sigma, X, Z, S, W, zeta)
+
+
+end
 
 
 # import pyBLP
 
-pyblp = PyCall("pyblp")
+pyblp = pyimport("pyblp")
 
 
 # call gradient
